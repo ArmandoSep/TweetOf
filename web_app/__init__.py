5e7623b4df20c79334683e6a41bb56950b0332b0
ArmandoSep <sepulveda.armando97@gmail.com>
2020-07-25 17:17:28 -0500

    Modified the init file

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -10,14 +10,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URI = os.getenv("DATABASE_URI")
+DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 
 def create_app():
     app = Flask(__name__)
 
     app.config["SECRET_KEY"] = SECRET_KEY
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     
     db.init_app(app)
